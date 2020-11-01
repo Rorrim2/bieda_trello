4cb5de4d03ac7c6908e73034c4f2a6b1067512a3
Rorrim2 <alisss07@gmail.com>
2020-11-01 19:46:03 +0100

    Testing deploy

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Welcome to the dark side, we have cookies</h1>
     <div v-for="user_ql in users">
       <UserBubble :name="user_ql.name" :id="user_ql.id" :lastName="user_ql.lastName"> </UserBubble>
     </div>
