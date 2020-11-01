355f75c0d8e6714bce4d7ec77819838401957cca
Rorrim2 <alisss07@gmail.com>
2020-11-01 20:37:05 +0100

    Switching prod and dev

--- src/vue-apollo.ts
+++ src/vue-apollo.ts
@@ -6,6 +6,8 @@ import VueApollo from 'vue-apollo'
 
 const httpLink = new HttpLink({
     // You should use an absolute URL here
+    //TODO
+    // uri: 'http://127.0.0.1:8000/graphql/',
     uri: 'https://bieda-trello-backend.herokuapp.com/graphql/',
 })
 
