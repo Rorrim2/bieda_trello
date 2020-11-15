d14170eb3003dd55851ec6c74a97fa5892b160ff
Rorrim2 <alisss07@gmail.com>
2020-11-15 19:25:11 +0100

    Adding cards to list, front style changes, added images, logo routes to home page

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,31 +1,31 @@
 <template>
   <div>
     <b-card-group deck><div v-for="list_s in listOfLists"><single-list :list="list_s"></single-list></div></b-card-group>
-  <div class="card bg-light text-primary" style="width: 15vw; margin: 3vw;" @submit="onCreate">
+  <div class="card text-light" style="width: 15vw; margin: 3vw; background-color: rgba(245,245,245, 0.3);" @submit="onCreate">
     <div class="card-body">
       <form>
         <p class="h3 text-center">Create list</p>
-        <!-- Default input name -->
-        <label for="defaultFormCardNameEx"  class="grey-text font-weight-light">List name</label>
+        <label for="defaultFormCardNameEx"  class="font-weight-light">List name</label>
         <input type="text" v-model="list.name" id="defaultFormCardNameEx" class="form-control">
         <br>
         <div class="text-center" style="margin: 1vw;">
-          <button class="btn bg-primary text-light" type="submit">Create list</button>
+          <button class="btn bg-secondary text-light" type="submit">Create list</button>
         </div>
       </form>
     </div>
-    <!-- Card body -->
   </div>
   </div>
 </template>
 
 <script>
 import SingleList from "@/components/SingleList";
+import SingleCard from "@/components/SingleCard";
 
 export default {
 name: "BoardView",
   components: {
-  SingleList
+    SingleList,
+    SingleCard,
   },
   data: () => ({
     listOfLists: [],
@@ -39,6 +39,7 @@ name: "BoardView",
       this.listOfLists.push({name: this.list.name, listOfCards: this.list.listOfCards})
       this.list.listOfCards = []
       this.list.name = ''
+      console.log("BoardView")
     }
   }
 }
