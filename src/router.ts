3c95bc895115eb45ac0f8a5fc23311dcf2a21a95
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-11-22 12:08:49 +0100

    changed route pattern for boards, implement re-usable function for route-guards of Login and Sign_up components

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,28 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import Router, {NavigationGuardNext, Route} from 'vue-router';
 import Home from './views/Home.vue';
 import Boards from './views/Boards.vue';
 import BoardView from './views/BoardView.vue';
-import {User} from "@/data_models/types";
 import {getToken, vm} from "@/main";
 
 Vue.use(Router);
+
+function checkIfLoginSignUpIsAllowed(to: Route, from: Route, next: NavigationGuardNext){
+    let item = localStorage.getItem('active_user');
+    let user = item ? JSON.parse(item) : item;
+    let tkn = getToken();
+    let r_tkn = vm? vm.$cookies.get('r_tkn') : "";
+
+    if((r_tkn || tkn ) && user ){
+        console.debug(from.path)
+        if(from.path !== `/u/${user.id}/boards`) {
+            next(`u/${user.id}/boards`);
+        }
+    }
+    else {
+        next();
+    }
+}
 
 export default new Router({
   routes: [
@@ -22,19 +38,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/Login.vue'),
-      beforeEnter: (to, from, next) => {
-          let item = localStorage.getItem('active_user');
-          let user = item ? JSON.parse(item) : item;
-          let tkn = getToken();
-          let r_tkn = vm? vm.$cookies.get('r_tkn') : "";
-
-          if((r_tkn || tkn ) && user){
-              next(`u/${user.id}/boards`);
-          }
-          else {
-              next();
-          }
-      }
+      beforeEnter: checkIfLoginSignUpIsAllowed
     },
     {
       path: '/signup',
@@ -43,9 +47,10 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/Sign_up.vue'),
+      beforeEnter: checkIfLoginSignUpIsAllowed
     },
     {
-      path: '/u/*/boards',
+      path: '/u/(.{16})/boards',
       name: 'boards',
       component: Boards,
       beforeEnter: (to, from, next) => {
