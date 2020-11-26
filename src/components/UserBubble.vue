1ec45f851328c217bd7c101cd72be8b3f0eeb5b1
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-11-26 16:27:21 +0100

    Updated UserBubble component, todo -> on logout clean User property.

--- src/components/UserBubble.vue
+++ src/components/UserBubble.vue
@@ -1,22 +1,51 @@
 <template>
-  <div :title="name + ' ' + lastName + ' ,id: ' + id">
-    <span>{{name.substring(0,1)}}{{lastName.substring(0,1)}}</span>
+  <div class="rounded-circle person bg-info" :title="title">
+    <span>{{shortenUser}}</span>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
+import {User} from "@/data_models/types";
+import {getFromLocalStorage} from "@/utils";
 
 @Component
 export default class UserBubble extends Vue{
-  @Prop() id!: string
-  @Prop() name!: string
-  @Prop() lastName!: string
+
+  public get user(): User {
+    return this._user;
+  }
+
+  public set user(value: User) {
+    console.debug(`user is from value: ${value.id} + ${value.lastName} + ${value.name}`)
+    this._user = value;
+  }
+
+  get shortenUser(): string{
+    return this._user.name.substring(0,1) + this._user.lastName.substring(0,1);
+  }
+
+  get title(): string{
+    return `${this._user.name} ${this._user.lastName} ,id: ${this._user.id}`;
+  }
+
+  created(){
+      if(!this.user){
+        this.user = <User> getFromLocalStorage("active_user") ?? <User> {};
+      }
+  }
+
+  private _user: User = <User> {};
 }
 
 
 </script>
 
 <style scoped>
-
+  .person{
+    min-width: 3vw;
+    min-height: 3vw;
+    text-align: center;
+    line-height: 3vw;
+  }
 </style>
