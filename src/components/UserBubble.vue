92b4a47d0dc6fccb8dc07920dbcab19604b8aa08
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-12-03 23:54:03 +0100

    Improved UserBubble component to attach dropdown menu on-click

--- src/components/UserBubble.vue
+++ src/components/UserBubble.vue
@@ -1,17 +1,43 @@
 <template>
-  <div class="rounded-circle person bg-info" :title="title">
-    <span>{{shortenUser}}</span>
+  <div>
+    <b-dropdown @hide="check_if_can_hide($event)" right no-caret no-flip toggle-class="rounded-circle px-0 py-0" ref="d_down" @show="show_overlay($event)">
+      <template #button-content>
+        <b-avatar :title="title" class="bg-secondary" :id="`user_menu`">
+          <span>{{ shortenUser }}</span>
+        </b-avatar>
+      </template>
+      <b-dropdown-header variant="success" class="m-0 p-0">
+        <div class="container-fluid m-0 p-0">
+          <p class="p-0 pl-5 m-0">Account</p>
+
+          <b-button @click="hide_overlay($event)"
+                    class="close pl-1 pb-2 pr-1"
+                    aria-label="Close">
+            <span aria-hidden="true" style="display: block;">&times</span>
+          </b-button>
+        </div>
+      </b-dropdown-header>
+      <b-dropdown-divider></b-dropdown-divider>
+      <b-dropdown-item>
+
+      </b-dropdown-item>
+    </b-dropdown>
+
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Prop, Vue} from 'vue-property-decorator';
 import {User} from "@/data_models/types";
+import { BDropdown } from 'bootstrap-vue';
 
-@Component
+@Component({
+  components: {}
+})
 export default class UserBubble extends Vue {
 
   @Prop() user!: User;
+  private show: boolean = false;
 
   get shortenUser(): string {
     return this.user.name.substring(0, 1) + this.user.lastName.substring(0, 1);
@@ -20,15 +46,32 @@ export default class UserBubble extends Vue {
   get title(): string {
     return `${this.user.name} ${this.user.lastName} ,id: ${this.user.id}`;
   }
+
+  hide_overlay(evt: Event) {
+    this.show = false;
+    const dropdown = this.$refs.d_down as BDropdown;
+    dropdown.hide();
+  }
+
+  check_if_can_hide(evt: Event){
+    if(this.show){
+      evt.preventDefault();
+    }
+  }
+
+  show_overlay(evt: Event) {
+    this.show = true;
+  }
+
 }
 </script>
 
 <style scoped>
-  .person{
-    min-width: 3vw;
-    min-height: 3vw;
-    text-align: center;
-    line-height: 3vw;
-    font-size: 1vw;
-  }
+header {
+  padding: 1px;
+  margin: 1px;
+  flex-direction: row;
+  flex: 1 1 content !important;
+}
+
 </style>
