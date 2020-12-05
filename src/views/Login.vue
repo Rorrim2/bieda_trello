9826e660da55323711ec3dbe1ba63a4795baf2a9
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-12-05 20:09:04 +0100

    Reformatted code, improved Login component with BootstrapVue components. Added temporarily welcome_back2.png, due to the fact that it has  better quality than welcome_back.png

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="about py-5">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 order-lg-2 order-2 align-self-center">
-          <form class="form-inline" @submit="onSubmit">
-            <div class="form-group">
-              <input type="email" v-model="credentials.email" class="form-control" id="inputmailinline" placeholder="E-mail"> </div>
-            <div class="form-group">
-              <input type="password" v-model="credentials.password" class="form-control mx-2" id="inputpasswordinline" placeholder="Password"> </div>
-            <button type="submit" class="btn text-white btn-secondary">Log in</button>
-          </form>
-          <b-form-invalid-feedback :state="loginValidation" v-text="loginError"></b-form-invalid-feedback>
-        </div>
-        <div class="col-lg-4 order-1 order-lg-1 p-0"> <img class="img-fluid d-block" src="../assets/welcome_back.png"></div>
-      </div>
-    </div>
+  <div class="about py-auto h-100 bg-primary">
+    <b-container class="flex-row h-100">
+      <b-col lg="4" class="order-1 order-lg-1 p-0 d-inline-flex">
+        <b-img fluid-grow src="../assets/welcome_back2.png"/>
+      </b-col>
+      <b-col lg="8" class="order-lg-2 order-2 p-0 align-self-center d-inline-flex">
+        <b-form inline @submit="onSubmit">
+          <b-input-group class="m-1">
+            <b-form-input type="email" required v-model="credentials.email" id="inputmailinline" placeholder="E-mail"/>
+          </b-input-group>
+          <b-input-group class="m-1">
+            <b-form-input type="password" required v-model="credentials.password" id="inputpasswordinline"
+                          placeholder="Password"/>
+          </b-input-group>
+          <b-button type="submit" class="text-white btn-secondary m-1">Log in</b-button>
+          <b-form-invalid-feedback :state="loginValidation" v-text="loginError"/>
+        </b-form>
+      </b-col>
+
+    </b-container>
 
   </div>
 </template>
@@ -34,49 +38,49 @@ export default class Login extends Vue {
   private loginError: string = "";
   private loginResult: AuthResult = <AuthResult>{};
 
-  get loginValidation(): boolean{
+  get loginValidation(): boolean {
     console.debug(`loginValidation is ${!this.loginError || this.loginError === ""}`)
     return !this.loginError || this.loginError === "";
   };
 
   mutate(cred: Credentials) {
-     const credentials = cred;
-     const component = this;
-     component.credentials = <Credentials>{};
+    const credentials = cred;
+    const component = this;
+    component.credentials = <Credentials>{};
 
-     this.$apollo.mutate({
-       mutation: LoginMutation,
-       variables: {
-         email: credentials.email,
-         password: credentials.password
-       },
-     }).then((data) => {
-       let authResult = <AuthResult>data.data.loginuser;
-       if (authResult.success) {
-         setToken(authResult.token)
-         let payload = parseJWT(authResult.token)
-         storeInLocalStorage("jti", payload.jti);
-         storeInLocalStorage("exp", payload.exp)
-         cacheRefreshToken(authResult.refreshToken)
-         storeInLocalStorage("active_user", authResult.user)
+    this.$apollo.mutate({
+      mutation: LoginMutation,
+      variables: {
+        email: credentials.email,
+        password: credentials.password
+      },
+    }).then((data) => {
+      let authResult = <AuthResult>data.data.loginuser;
+      if (authResult.success) {
+        setToken(authResult.token)
+        let payload = parseJWT(authResult.token)
+        storeInLocalStorage("jti", payload.jti);
+        storeInLocalStorage("exp", payload.exp)
+        cacheRefreshToken(authResult.refreshToken)
+        storeInLocalStorage("active_user", authResult.user)
 
-         component.loginResult = authResult;
-         dataBus.$emit('updateUser');
+        component.loginResult = authResult;
+        dataBus.$emit('updateUser');
 
-         component.$router.push(`u/${component.loginResult.user.id}/boards`)
-       }
-     }).catch((error) => {
-       console.debug(error)
-       component.credentials = credentials
-       component.loginError = error.message;
-     })
-   };
+        component.$router.push(`u/${component.loginResult.user.id}/boards`)
+      }
+    }).catch((error) => {
+      console.debug(error)
+      component.credentials = credentials
+      component.loginError = error.message;
+    })
+  };
 
-    public onSubmit (evt: Event)  {
-      evt.preventDefault();
-      let loginData = this.credentials;
-      this.mutate(loginData);
-    };
+  public onSubmit(evt: Event) {
+    evt.preventDefault();
+    let loginData = this.credentials;
+    this.mutate(loginData);
+  };
 };
 
 
