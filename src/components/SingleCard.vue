7d383b6e66478ddadafceece64e8f22a2174993e
Hergoln <juliusz.malka@o2.pl>
2021-01-06 21:39:45 +0100

    fixed editCardsTitleAndDescription function and edit of title and description in singleCard

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,76 +1,108 @@
 <template>
-  <div>
-    <form v-if="this.creatingCard" @submit.prevent="onCardCreate">
-      <label :for="editCardModalName"  class="grey-text font-weight-light">Card text</label>
-      <input type="text" v-model="card.title" :id="editCardModalName" class="form-control">
-      <br>
-      <div class="text-center" style="margin: 1vw;">
-        <button class="btn bg-primary text-light" type="submit">Save</button>
-      </div>
-    </form>
-    <div v-else>
+    <div>
       <b-button variant="light" style="margin: 2px;" v-b-modal = "editCardModalName">{{card.title}}</b-button>
       <b-modal :id=editCardModalName ok-only>
-<!--        :title=card.text-->
         <template #modal-header="{ close }">
-          <div>
-            <h4 class="modal-title">{{ card.title }}</h4>
-            <b-button variant="danger" type="button" @click="close()">
-              Close
-            </b-button>
-            <b-button variant="secondary" type="button" >
-              Edit title
-            </b-button>
-          </div>
+          <b-button v-show="!isEditingTitle" v-text="card.title"
+                    class="text-truncate text-nowrap btn border-0 bg-transparent h2 text-dark m-0 p-0 font-weight-bold"
+                    @click="editTitle($event)">
+          </b-button>
+          <b-form v-show="isEditingTitle" @focusout="onTitleSubmit($event)" @submit.prevent="onTitleSubmit($event)">
+            <b-form-input type="text" class="mr-1" v-model="card.title" ref="cardTitle"
+                          style="max-width: 150px;"/>
+          </b-form>
         </template>
-
         <h3>Description</h3>
-        <div v-if="this.editing">
-
-            <b-form-textarea
-                id="textarea" v-model="inputText" rows="3" max-rows="6"
-            ></b-form-textarea>
-          <b-button variant="success" @click="onSaveButton">Save</b-button>
-          <b-button variant="warning" @click="onCancelButton">Cancel</b-button>
+        <div v-if=this.isEditingDescription @focusout="onDescriptionSubmit($event)">
+            <b-form-input v-model="card.description" rows="3" max-rows="6"></b-form-input>
+          <b-button variant="success" @click="onDescriptionSubmit($event)">Save</b-button>
+          <b-button variant="warning" @click="onCancelButton($event)">Cancel</b-button>
         </div>
         <div v-else>
-          <p class="my-4">{{this.descriptionText}}</p>
-
-          <b-button variant="secondary" @click="onEditButton">Edit</b-button>
+          <p class="my-4">{{card.description}}</p>
+          <b-button variant="secondary" @click="editDescription($event)">Edit</b-button>
         </div>
 
       </b-modal>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
 
 import {Component, Prop, Vue} from "vue-property-decorator";
 import {SingleCardModel} from "@/data_models/types";
+import {BFormInput} from "bootstrap-vue";
+import {editCardsTitleAndDescription} from "@/utils/db_operations/cards";
 
 @Component
 export default class SingleCard extends Vue{
   @Prop() card!: SingleCardModel;
 
   private editCardModalName = 'edit-modal-card' + this.card.id;
-  private creatingCard: boolean = true;
-  private descriptionText: string = "Enter some text";
-  private editing: boolean = true;
-  private inputText: string = '';
-  onCardCreate() {
-      this.creatingCard = false
+  private isEditingTitle: boolean = false;
+  private isEditingDescription: boolean = false;
+  private cardName: string = '';
+  private description: string = '';
+
+  updateCard() {
+    editCardsTitleAndDescription(this.card, data => {
+      this.card = data.card
+    }, error => {
+      console.log(error)
+    });
   }
-  onEditButton() {
-      this.inputText = this.descriptionText
-      this.editing = true
+
+  editTitle(event: Event) {
+    this.isEditingTitle = true;
+    this.cardName = String(this.card.title);
+    this.$nextTick(() => {
+      const elem: BFormInput = <BFormInput>this.$refs.cardTitle;
+      elem.focus();
+      console.log(elem);
+    })};
+
+  onTitleSubmit(event: Event) {
+    event.preventDefault();
+    this.isEditingTitle = false;
+    console.log(this.card.title);
+
+    if (this.card.title == undefined || this.card.title === "") {
+      this.card.title = this.cardName;
+    }
+
+    if(this.card.title === this.cardName) {
+      this.cardName = "";
+      return;
+    }
+    this.cardName = "";
+    this.updateCard();
   }
-  onSaveButton() {
-      this.descriptionText = this.inputText
-      this.editing = false
+
+  editDescription(event: Event) {
+    this.isEditingDescription = true;
+    this.description = String(this.card.description)
+  };
+
+  onDescriptionSubmit(event: Event) {
+    event.preventDefault();
+    this.isEditingDescription = false;
+    console.log(this.card.description);
+
+    if (this.card.description == undefined || this.card.description === "") {
+      this.card.description = this.description;
+    }
+
+    if(this.card.description === this.description) {
+      this.description = "";
+      return;
+    }
+    this.description = "";
+    this.updateCard();
   }
-  onCancelButton() {
-      this.editing = false
+
+  onCancelButton(event: Event) {
+    this.isEditingDescription = false;
+    this.description = '';
   }
 }
 </script>
