36749cc5c27294dc6c3f77e15c45eb070aa781ac
Kamil Chrustowski <chrustek123456789@gmail.com>
2021-01-07 13:49:08 +0100

    Improved SingleList component

--- src/components/SingleList.vue
+++ src/components/SingleList.vue
@@ -1,16 +1,42 @@
 <template>
   <b-card
-      style="background-color: rgba(245,245,245, 0.3);"
-      text-variant="light">
+      style="background-color: rgba(200,200,200, 0.6);"
+      text-variant="dark">
     <template #header>
-      <b-row>
-        <h4 class="text-truncate">{{ listModel.title }}</h4>
-        <b-dropdown text="..." no-caret right>
-          <b-dropdown-item class="m-1 p-1" text-variant="light" v-b-modal.modal-change-list-name>Edit list name
+      <b-row align-h="between" align-v="center">
+        <h6 :title="listModel.title" class="ml-2 text-truncate font-weight-bold" style="max-width: 202px;" >{{ listModel.title }}</h6>
+        <b-dropdown variant="dark" no-caret right toggle-class="bg-transparent">
+          <template #button-content>
+            <b-icon icon="three-dots-vertical">
+            </b-icon>
+          </template>
+          <b-dropdown-item class="m-1 p-1" text-variant="light"
+                           v-b-modal.modal-change-list-name>Edit list name
           </b-dropdown-item>
         </b-dropdown>
       </b-row>
     </template>
+
+    <template #footer>
+      <b-button v-if="!creatingCard" @click=onAddCardClick variant="secondary"
+                style="margin: 1vw;" type="button" v-text="`Add Card`"/>
+      <form v-else @submit.prevent="onCreateCard">
+        <label :for=editCardModalName  class="grey-text font-weight-light">Card text</label>
+        <input type="text" v-model="newCardTitle" :id=editCardModalName class="form-control">
+        <div class="text-center" style="margin: 1vw;">
+          <input type="submit" value="Save" class="btn bg-primary text-light">
+        </div>
+      </form>
+    </template>
+
+    <div class="overflow-y">
+        <Container>
+          <Draggable v-for="card_s in listModel.cards" :key="card_s.id">
+            <single-card :card="card_s"></single-card>
+          </Draggable>
+        </Container>
+    </div>
+
     <b-modal id="createnewcardmodal" class="bg-light"
              title="Create Card" @ok="modalOk($event)">
       <b-form @submit.prevent="onCreateCard">
@@ -31,28 +57,21 @@
         </b-form-group>
       </b-form>
     </b-modal>
-    <div class="card-body">
-      <div v-for="card_s in listModel.cards">
-        <single-card :card="card_s"></single-card>
-      </div>
 
-      <b-button v-if="!this.creatingCard" @click=onAddCardClick variant="secondary"
-                style="margin: 1vw;" type="button" v-text="`Add Card`"/>
-      <form v-else @submit.prevent="onCreateCard">
-        <label :for=editCardModalName  class="grey-text font-weight-light">Card text</label>
-        <input type="text" v-model="newCardTitle" :id=editCardModalName class="form-control">
-        <div class="text-center" style="margin: 1vw;">
-          <input type="submit" value="Save" class="btn bg-primary text-light">
-        </div>
-      </form>
-
-    </div>
   </b-card>
 </template>
-
+<style scoped>
+.card-body{
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-bottom: 1px;
+  padding-top: 1px;
+}
+</style>
 <script lang="ts">
 import SingleCard from "@/components/SingleCard.vue";
-import {Component, Prop, Vue} from "vue-property-decorator";
+import {Draggable, Container} from "vue-smooth-dnd";
+import {Component, Prop, Vue, Watch} from "vue-property-decorator";
 import {
   dummySingleCardModel,
   dummySingleListModel,
@@ -65,16 +84,12 @@ import {fetchList} from '@/utils/db_operations/lists';
 
 @Component({
   components: {
-    SingleCard
-  },
-  watch: {
-    list: {
-      handler(val: SingleListPreview, oldVal: SingleListPreview) {
-        (<SingleList>this).listChanged();
-      }
-    }
+    SingleCard,
+    Draggable,
+    Container
   }
 })
+
 export default class SingleList extends Vue {
   @Prop() list!: SingleListPreview;
   private name: string = "";
@@ -90,6 +105,7 @@ export default class SingleList extends Vue {
     this.listChanged();
   }
 
+  @Watch('list', { immediate: true, deep: true })
   listChanged() {
     fetchList(this.list.id, data => {
       this.listModel = data;
@@ -98,6 +114,8 @@ export default class SingleList extends Vue {
       console.log(error.message);
     })
   }
+
+
 
   onAddCardClick(event: Event) {
     this.creatingCard = !this.creatingCard;
